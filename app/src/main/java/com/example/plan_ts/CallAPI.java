ef84fb915c9693b42eafb9d85799bba0468a5736
Samuel <samuel.pernthaler@gmail.com>
2021-06-10 12:26:19 +0200

    start api

---
+++ app/src/main/java/com/example/plan_ts/CallAPI.java
@@ -0,0 +1,41 @@
+package com.example.plan_ts;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class CallAPI extends AsyncTask<String, String, String> {
+    private String urlString;
+    private String data;
+
+    public CallAPI(String url, String data){
+        this.urlString = url;
+        this.data = data;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        OutputStream out = null;
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            out = new BufferedOutputStream(urlConnection.getOutputStream());
+
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
+            writer.write(data);
+            writer.flush();
+            writer.close();
+            out.close();
+
+            urlConnection.connect();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return "";
+    }
+}
--- app/src/main/java/com/example/plan_ts/MainActivity.java
+++ app/src/main/java/com/example/plan_ts/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.plan_ts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        text = findViewById(R.id.textView);
+
+        CallAPI callAPI = new CallAPI(text.toString(), text.toString());
     }
 }
