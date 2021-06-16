40bb13654b860aae734d1a2ccae28f359a221057
Samuel <samuel.pernthaler@gmail.com>
2021-06-16 16:52:51 +0200

    New Plant and Detail Plant

--- app/src/main/java/com/example/plan_ts/NewPlant.java
+++ app/src/main/java/com/example/plan_ts/NewPlant.java
@@ -3,11 +3,28 @@ package com.example.plan_ts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 public class NewPlant extends AppCompatActivity {
+    private Button newPlant_back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+        getSupportActionBar().hide(); //hide the title bar
+        getSupportActionBar().hide(); //hide the title bar
         setContentView(R.layout.activity_new_plant);
+
+        newPlant_back = findViewById(R.id.newPlant_back);
+        newPlant_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/plan_ts/PlantDetail.java
+++ app/src/main/java/com/example/plan_ts/PlantDetail.java
@@ -3,6 +3,7 @@ package com.example.plan_ts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class PlantDetail extends AppCompatActivity {
     public static final String PLANT_KEY = "Plant1";
@@ -10,6 +11,8 @@ public class PlantDetail extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+        getSupportActionBar().hide(); //hide the title bar
         setContentView(R.layout.activity_plant_detail);
     }
 }
