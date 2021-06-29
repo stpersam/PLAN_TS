6677527ea3226065c961aa3cb497e42159c6cff7
samuel <samue.pernthaler@gmail.com>
2021-06-29 17:26:16 +0200

    .

--- app/src/main/java/com/example/plan_ts/AddNewGroupView.java
+++ app/src/main/java/com/example/plan_ts/AddNewGroupView.java
@@ -2,22 +2,27 @@ package com.example.plan_ts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
+import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class AddNewGroupView extends AppCompatActivity {
@@ -29,7 +34,6 @@ public class AddNewGroupView extends AppCompatActivity {
     private EditText nameET;
     private EditText beschreibungET;
     private Button selectPlants;
-    private LinearLayout GroupScrollView;
     private Button Group_back;
 
     private List<Gruppe> gruppen = new ArrayList<>();
@@ -98,15 +102,14 @@ public class AddNewGroupView extends AppCompatActivity {
 
     public void onButtonShowPopupWindowClick(View view) {
 
-        userPflanzen = new ArrayList<>();
         // inflate the layout of the popup window
         LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
         View popupView = inflater.inflate(R.layout.selectplants_group_popup, null);
         Button selectBtn = popupView.findViewById(R.id.SelectBtn);
 
         // create the popup window
-        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
-        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int width = ScrollView.LayoutParams.WRAP_CONTENT;
+        int height = ScrollView.LayoutParams.WRAP_CONTENT;
         boolean focusable = true; // lets taps outside the popup also dismiss it
         final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
 
@@ -159,5 +162,45 @@ public class AddNewGroupView extends AppCompatActivity {
         });
     }
 
-    public void SelectedPlants(){}
+    public void SelectedPlants(){
+        ScrollView scrollView = findViewById(R.id.GroupScrollView);
+
+        for (int i = 0; i < selectedPlants.size(); i++) {
+            //LinearLayout
+            LinearLayout linearLayout = new LinearLayout(this);
+            linearLayout.setOrientation(LinearLayout.VERTICAL);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            layoutParams.gravity = Gravity.CENTER;
+            layoutParams.setMargins(50, 20, 50, 20);
+            linearLayout.setLayoutParams(layoutParams);
+            linearLayout.setId(i);
+
+            //TableLayout
+            TableLayout mTableLayout = new TableLayout(this);
+            mTableLayout.setLayoutParams(new TableLayout.LayoutParams(350, TableLayout.LayoutParams.WRAP_CONTENT));
+            mTableLayout.setGravity(Gravity.CENTER_HORIZONTAL);
+
+            for (int l = 0; l <= selectedPlants.size(); ) {
+                TableRow a = new TableRow(this);
+                TableRow.LayoutParams param = new TableRow.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT, 0.1f);
+                a.setLayoutParams(param);
+                a.setGravity(Gravity.CENTER_HORIZONTAL);
+
+                for (int y = 0; (y < 2) && (l < selectedPlants.size()); y++) {
+                    Button x = new Button(this);
+                    x.setText("TEMPtest");
+                    TableRow.LayoutParams par = new TableRow.LayoutParams(350, 350, 0);
+                    x.setLayoutParams(par);
+                    int ids = selectedPlants.get(l);
+                    String idsPl = selectedPlants.get(l).toString();
+                    x.setId(ids);
+                    a.addView(x);
+                    l++;
+                }
+                mTableLayout.addView(a);
+            }
+            linearLayout.addView(mTableLayout);
+            scrollView.addView(linearLayout);
+        }
+    }
 }
